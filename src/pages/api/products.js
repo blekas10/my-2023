97271813ef7385c2f09653810fcd54e75443a8b1
unknown <nojus.stankevicius@gmail.com>
2023-04-05 14:56:17 +0300

    fetching products with getserversideprop

--- src/pages/api/products.js
+++ src/pages/api/products.js
@@ -1,8 +1,12 @@
 import {initMongoose} from 'lib/mongoose.js';
 import Product from 'models/product';
 
+export async function findAllProducts() {
+    return Product.find().exec();
+}
+
 export default async function handle(req, res){
     await initMongoose();
-   res.json( await Product.find().exec())
+   res.json( await findAllProducts());
    
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,35 +4,24 @@ import { Inter } from 'next/font/google';
 import ProductCard from '../../components/product-component';
 
 import { useEffect, useState } from 'react';
+import { initMongoose } from '../../lib/mongoose';
+import { findAllProducts } from './api/products';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
-  const [productsInfo, setProductsInfo] = useState([]);
-  const [phrase, setPhrase] = useState([])
-  useEffect(() => {
-    fetch('api/products')
-      .then(response => {
-        if (typeof response !== 'object') {
-          throw new Error('Response is not an object');
-        }
-        return response.json();
-      })
-      .then(json => setProductsInfo(json))
-      .catch(error => console.error('Error fetching products:', error));
-  }, []);
+export default function Home({products}) {
 
-  const categoriesNames = [...new Set(productsInfo.map(p => p.category))]
+  const [phrase, setPhrase] = useState([]);
 
-  let products;
+  const categoriesNames = [...new Set(products.map(p => p.category))];
+
+
   if (phrase) {
-    products = productsInfo.filter(p => p.name.toLowerCase().includes(phrase))
-  } else {
-    products = productsInfo
+    products = products.filter(p => p.name.toLowerCase().includes(phrase))
   }
 
   return (
-    <>
+    <div className='mx-5'>
       <input value={phrase} onChange={e => setPhrase(e.target.value)} type="text" placeholder="Search for products..." className='bg-grey-100 w-full py-2 px-4 rounded-xl'></input>
 
       <div>
@@ -59,7 +48,7 @@ export default function Home() {
       <div className='bg-blue-100 py-4'></div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            {productsInfo.map(product => (
+            {products.map(product => (
             <div key={product._id} className="max-w-sm rounded overflow-hidden shadow-lg">
                 <img src={`/${product.image}`} alt="" className="w-full"></img>
                 <div className="px-6 py-4">
@@ -75,6 +64,19 @@ export default function Home() {
             </div>
             ))}
       </div>
-    </>
+
+      <footer>navigation</footer>
+    </div>
   )
+}
+
+
+export async function getServerSideProps() {
+  await initMongoose();
+  const products = await findAllProducts();
+  return {
+    props: {
+      products: JSON.parse(JSON.stringify(products)),
+    }
+  };
 }
