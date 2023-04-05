9e24907840c6aa6d89f0b894ac5ab7f5c1bcad52
unknown <nojus.stankevicius@gmail.com>
2023-04-05 16:19:04 +0300

    added footer, started checkout page

---
+++ components/Layout.js
@@ -0,0 +1,7 @@
+export default function Layout({children}) {
+    return (
+        <div className="p-5">
+            {children}
+        </div>
+    );
+}
---
+++ src/pages/checkout.js
@@ -0,0 +1,13 @@
+import Footer from "components/Footer";
+import Layout from "components/Layout";
+
+export default function CheckoutPage() {
+    return (
+        <>
+            <Layout>
+                Checkout
+            </Layout>
+            <Footer></Footer>
+        </>
+    );
+}
